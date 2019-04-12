fc14a5711e74fa5aecf291170403082e454070f7
Giovanna Maroniene <giovannamaroniene@gmail.com>
2019-04-12 16:16:41 -0400

    commit 1 lab3

--- lab2/App.js
+++ lab2/App.js
@@ -1,57 +1,81 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity, Text, View, TextInput, Button } from 'react-native';
-
+import { StyleSheet, Text, View, TextInput,Button,TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props);
+    this.state={value: ''};
+
     this.onPress = this.onPress.bind(this);
-      }
-      onChange(event) {
-        console.log(event);
-      }
 
-      onPress() {
-        console.log("Pressed");
-      }
-      render() {
-        return (
-          <View style={styles.container} flexDirection="column" alignItems='stretch'>
-            <View><TextInput style={styles.textInput} onChangeText={this.onChange} placeholder="Enter your name"></TextInput></View>
-            <TouchableOpacity style={styles.buttonStyle} onPress={this.onPress}><Text style={styles.buttonText}>Submit</Text></TouchableOpacity>
-          </View>
-        );
-      }
-    }
+  }
+  onChange = (text) => {
+      this.setState({ value:text });
+   }
 
-    const styles = StyleSheet.create({
-      buttonText:
-      {
-        color:"white",
-        fontSize:40
-      },
-      buttonStyle:
-      {
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor:'blue',
-        height:75,
-        margin:30,
-      },
-      textInput:
-      {
-        margin:30,
-        height:75,
-        fontSize:20
-      },
-      defaultText:
-      {
-        fontSize:20
-      },
-      container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-    });
+  onPress= (text) =>  {
+    if(/^[a-zA-Z]+$/.test(this.state.value)){
+    this.setState({nameAvailable:true})
+  } else{
+    this.setState({isValid:true})
+    this.setState({error:(<Text style={styles.errorText}>Please enter a valid name</Text>)})
+
+   };
+
+  }
+  render() {
+    if (!this.state.nameAvailable){
+    return (
+      <View style={styles.container}>
+      <View><TextInput style={styles.textInput} onChangeText={this.onChange} placeholder="Enter name here"></TextInput></View>
+        <TouchableOpacity style={styles.buttonStyle} onPress={this.onPress}><Text style={styles.buttonText}>Submit</Text></TouchableOpacity>
+        {!this.state.valid ? (<Text style={styles.errorText}>{this.state.error}</Text>) : null}
+        </View>
+    );
+  } else{
+    return(
+      <View style={styles.container}>
+      {!this.state.valid ? (<Text style={styles.defaultText}>Hello {this.state.value}</Text>) : null}
+      </View>
+    )
+  }
+  }
+}
+
+
+const styles = StyleSheet.create({
+  buttonText:
+{
+  color:"white",
+  fontSize:20
+},
+buttonStyle:
+{
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor:'#424B54',
+  height:45,
+  margin:30,
+},
+textInput:
+{
+  margin:30,
+  height:75,
+  fontSize:20,
+},
+defaultText:
+{
+  fontSize:20
+},
+errorText:
+{
+  fontSize:20,
+  color:'red',
+},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- lab3/screens/HomeScreen.js
+++ lab3/screens/HomeScreen.js
@@ -17,20 +17,20 @@ export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
-  _gotoScreen = (key) => {
-    console.log("Going to " + key);
-  }
-  render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.getStartedContainer}>
-            <Text style={styles.getStartedText}>Cats are amazing</Text>
-            <FlatList
-             data={[{key: 'cat1',image: require('../assets/images/cat1.png')}, {key: 'cat2',image: require('../assets/images/cat2.png')}]}
+
+  render() { const {navigate} = this.props.navigation;
+    return (<View style={styles.container}>
+      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <View style={styles.getStartedContainer}>
+        <Text style={styles.getStartedText}>Enter Images</Text>
+        <FlatList              data={[{key: 'cat1',
+                     image: require('../assets/images/cat1.png')},
+                    {key: 'cat2',
+                     image: require('../assets/images/cat2.png')},
+                     {key: 'cat3',
+                      image: require('../assets/images/cat3.jpg')}]}
              keyExtractor={this._keyExtractor}
-              renderItem={({item}) => <TouchableOpacity onPress={(event) => { console.log(item.key) }}>
+              renderItem={({item}) => <TouchableOpacity onPress={(event) => navigate('Detail')}>
                 <Image source={item.image} style={{width:200,height:200}} />
               </TouchableOpacity>}
             />
@@ -40,6 +40,40 @@ export default class HomeScreen extends React.Component {
     );
   }
 
+
+
+  _maybeRenderDevelopmentModeWarning() {
+    if (__DEV__) {
+      const learnMoreButton = (
+        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+          Learn more
+        </Text>
+      );
+
+      return (
+        <Text style={styles.developmentModeText}>
+          Development mode is enabled, your app will be slower but you can use useful development
+          tools. {learnMoreButton}
+        </Text>
+      );
+    } else {
+      return (
+        <Text style={styles.developmentModeText}>
+          You are not in development mode, your app will run at full speed.
+        </Text>
+      );
+    }
+  }
+
+  _handleLearnMorePress = () => {
+    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+  };
+
+  _handleHelpPress = () => {
+    WebBrowser.openBrowserAsync(
+      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+    );
+  };
 }
 
 const styles = StyleSheet.create({
