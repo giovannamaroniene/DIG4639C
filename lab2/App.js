3db010c8f426f28982aaa794489e1f1d9267965e
Giovanna Maroniene <giovannamaroniene@gmail.com>
2019-04-12 17:32:17 -0400

    lab2 app file

--- lab2/App.js
+++ lab2/App.js
@@ -1,77 +1,101 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput,Button,TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={value: ''};
-
+    this.state = {value: '', correctname: true, storage: ''};
+    this.setName = this.setName.bind(this);
     this.onPress = this.onPress.bind(this);
-
+    this.onChange = this.onChange.bind(this);
   }
-  onChange = (text) => {
-      this.setState({ value:text });
-   }
 
-  onPress= (text) =>  {
-    if(/^[a-zA-Z]+$/.test(this.state.value)){
-    this.setState({nameAvailable:true})
-  } else{
-    this.setState({isValid:true})
-    this.setState({error:(<Text style={styles.errorText}>Please enter a valid name</Text>)})
-
-   };
-
+  setName(name) {
+    this.setState({storage:name})
   }
+
+  onChange(event) {
+    this.setState({value:event});
+    console.log(event);
+  }
+
+  onPress(event) {
+    if (/[^A-Za-z]/.test(this.state.value)){
+      this.setState({correctname: false, value:''});
+    } else {
+      this.setName(this.state.value);
+    }
+
+    event.preventDefault();
+  }
+
   render() {
-    if (!this.state.nameAvailable){
     return (
-      <View style={styles.container}>
-      <View><TextInput style={styles.textInput} onChangeText={this.onChange} placeholder="Enter name here"></TextInput></View>
-        <TouchableOpacity style={styles.buttonStyle} onPress={this.onPress}><Text style={styles.buttonText}>Submit</Text></TouchableOpacity>
-        {!this.state.valid ? (<Text style={styles.errorText}>{this.state.error}</Text>) : null}
+      <View style={styles.container} flexDirection="column" alignItems='stretch'>
+      {(this.state.storage == '') ? <View style={styles.container} flexDirection="column" alignItems='stretch'>
+
+        <View style={styles.container} allignItems="stretch">
+        <TextInput style={styles.textInput} value={this.state.value} onChangeText={this.onChange} placeholder="Your name">
+        </TextInput>{this.state.correctname ? null: <Text className="incorrect" style={styles.defaultText2}>Invalid. Use only letters.</Text>}
         </View>
-    );
-  } else{
-    return(
-      <View style={styles.container}>
-      {!this.state.valid ? (<Text style={styles.defaultText}>Hello {this.state.value}</Text>) : null}
+
+        <TouchableOpacity style={styles.buttonStyle} onPress={this.onPress}>
+        <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+
       </View>
-    )
-  }
+    :  <View>
+        <TouchableOpacity style={styles.buttonStyle2} onPress={()=>this.setState({storage: '', value:''})}>
+        <Text style={styles.buttonText}>Go Back</Text>
+        </TouchableOpacity>
+        <Text className="incorrect" style={styles.defaultText}>Welcome, {this.state.storage}!</Text></View>}
+      </View>
+    );
   }
 }
-
-
 const styles = StyleSheet.create({
   buttonText:
-{
-  color:"white",
-  fontSize:20
-},
-buttonStyle:
-{
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor:'#424B54',
-  height:45,
-  margin:30,
-},
-textInput:
-{
-  margin:30,
-  height:75,
-  fontSize:20,
-},
-defaultText:
-{
-  fontSize:20
-},
-errorText:
-{
-  fontSize:20,
-  color:'red',
-},
+  {
+    color:"white",
+    fontSize:40
+  },
+  buttonStyle:
+  {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'blue',
+    height:75,
+    margin:30,
+  },
+  buttonStyle2:
+  {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'green',
+    height:65,
+    margin:30,
+  },
+  textInput:
+  {
+    margin:30,
+    height:75,
+    fontSize:20
+  },
+  defaultText:
+  {
+    fontSize:20,
+    color:'green',
+    justifyContent: 'center',
+    textAlign: 'center',
+    marginTop:50
+  },
+  defaultText2:
+  {
+    fontSize:20,
+    color:'red',
+    alignItems: 'stretch',
+    textAlign: 'center'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
